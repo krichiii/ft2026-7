b1a13b62a412333eedbbe6f9a668ed6118519827
Stanislav bc2025 <krichi306@gmail.com>
2026-04-01 15:31:55 +0300

    added inventory service

---
+++ src/services/inventoryApi.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:5000'; // Default backend URL
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+export const inventoryApi = {
+  getInventory: () => api.get('/inventory'),
+  getInventoryById: (id) => api.get(`/inventory/${id}`),
+  createInventory: (formData) => api.post('/register', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  }),
+  updateInventoryText: (id, data) => api.put(`/inventory/${id}`, data),
+  updateInventoryPhoto: (id, formData) => api.put(`/inventory/${id}/photo`, formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  }),
+  deleteInventory: (id) => api.delete(`/inventory/${id}`),
+};
+
+export default api;
